6f5b81d73913d2d55c68b7277d25377047f35745
guifischeer <guilherme_florent@estudante.sesisenai.org.br>
2025-11-04 08:36:42 -0300

    COMMIt porta

--- db_connect.php
+++ db_connect.php
@@ -1,11 +1,11 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "";
 $dbname = "kanban";
-$port = 4333; 
+$port = 3307; 
 
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+$conn = new mysqli('127.0.0.1', $username, $password, $dbname, 3307);
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
